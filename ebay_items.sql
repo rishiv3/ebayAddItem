6991b2b9f53deb1ddce43ab5de75b598f3549d52
Rishi Vishwakarma <rishi_vishwakarma@hotmail.com>
2020-08-26 19:01:54 +0530

    ebayitem to content

--- ebay_items.sql
+++ ebay_items.sql
@@ -19,10 +19,8 @@ CREATE TABLE IF NOT EXISTS `ebay_items` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci ;
 
-
 CREATE TABLE `content` (
   `id` int(10) UNSIGNED NOT NULL,
-  `asset_id` int(10) UNSIGNED NOT NULL DEFAULT 0 COMMENT 'FK to the #__assets table.',
   `title` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '',
   `alias` varchar(400) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL DEFAULT '',
   `introtext` mediumtext COLLATE utf8mb4_unicode_ci NOT NULL,
@@ -30,14 +28,10 @@ CREATE TABLE `content` (
   `state` tinyint(3) NOT NULL DEFAULT 0,
   `catid` int(10) UNSIGNED NOT NULL DEFAULT 0,
   `created` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
-  `created_by` int(10) UNSIGNED NOT NULL DEFAULT 0,
+  `created_by` varchar(20) NOT NULL DEFAULT 0,
   `created_by_alias` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '',
   `modified` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
-  `modified_by` int(10) UNSIGNED NOT NULL DEFAULT 0,
-  `checked_out` int(10) UNSIGNED NOT NULL DEFAULT 0,
-  `checked_out_time` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
-  `publish_up` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
-  `publish_down` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `modified_by` varchar(20) NOT NULL DEFAULT 0,
   `images` text COLLATE utf8mb4_unicode_ci NOT NULL,
   `urls` text COLLATE utf8mb4_unicode_ci NOT NULL,
   `attribs` varchar(5120) COLLATE utf8mb4_unicode_ci NOT NULL,
@@ -47,39 +41,15 @@ CREATE TABLE `content` (
   `metadesc` text COLLATE utf8mb4_unicode_ci NOT NULL,
   `access` int(10) UNSIGNED NOT NULL DEFAULT 0,
   `hits` int(10) UNSIGNED NOT NULL DEFAULT 0,
-  `metadata` text COLLATE utf8mb4_unicode_ci NOT NULL,
-  `featured` tinyint(3) UNSIGNED NOT NULL DEFAULT 0 COMMENT 'Set if article is featured.',
-  `language` char(7) COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'The language code for the article.',
-  `xreference` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '' COMMENT 'A reference to enable linkages to external data sets.',
-  `note` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT ''
+  `metadata` text COLLATE utf8mb4_unicode_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
---
--- Indexes for dumped tables
---
 
---
--- Indexes for table `content`
---
 ALTER TABLE `content`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `idx_access` (`access`),
-  ADD KEY `idx_checkout` (`checked_out`),
   ADD KEY `idx_state` (`state`),
-  ADD KEY `idx_catid` (`catid`),
-  ADD KEY `idx_createdby` (`created_by`),
-  ADD KEY `idx_featured_catid` (`featured`,`catid`),
-  ADD KEY `idx_language` (`language`),
-  ADD KEY `idx_xreference` (`xreference`),
-  ADD KEY `idx_alias` (`alias`(191));
+  ADD KEY `idx_catid` (`catid`);
 
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `content`
---
 ALTER TABLE `content`
   MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
 COMMIT;
